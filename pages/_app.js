7f8c74b2340ded01038bdc302bc26f69f745e52f
Raul Alcantara <ralcanta@mit.edu>
2021-01-07 12:54:07 -0500

    added _app to include css in next app

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles.css'
+import '../generalNav.css'
+import '../utilities.css'
+import "../components/videoSection.css"
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
