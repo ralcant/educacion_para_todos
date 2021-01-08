c0ce56b5899dbe85ecbe2ff2ac109f03f86cc08e
Raul Alcantara <ralcanta@mit.edu>
2021-01-08 13:14:17 -0500

    added style for clicking favorite videos

--- components/youtubeVideo.js
+++ components/youtubeVideo.js
@@ -8,11 +8,19 @@ export default class YoutubeVideo extends React.Component{
         // this.state = {
         //     elements: this.props.elements;
         // }
+        this.state = {
+            checked: false, // to see if it's a favorite
+        }
 
+    }
+    checked = ()=>{
+        this.setState({
+            checked: !this.state.checked,
+        })
     }
     render(){
         return(
-            <div className="box-container center ">
+            <div className={`box-container center ${this.state.checked && 'boxFavorite'}`} onClick={this.checked}>
                 <section className="two-flex">
                     <h2><strong>{this.props.info.title}</strong></h2> 
                     <p>{this.props.info.description}</p>
